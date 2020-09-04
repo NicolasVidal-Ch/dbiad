db7e49e3708bbdb76893280851aebe4e173aa747
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-09-04 15:11:29 +0200

    Update dbiad1.sh

--- dbiad1.sh
+++ dbiad1.sh
@@ -9,8 +9,8 @@ nom=dopt380$varr
 # Edition fichier hostname
 > /etc/hostname
 echo $nom.bt8it.afpa  >> /etc/hostname
-echo $nom            >> /etc/hostname
-echo bt8it.afpa          >> /etc/hostname
+echo $nom >> /etc/hostname
+echo bt8it.afpa >> /etc/hostname
 
 
 # Edition fichier hosts
